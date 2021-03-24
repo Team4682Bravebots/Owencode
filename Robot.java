88f8a224e2c5f1ca442ee1d56a9b0c427d6c499b
Frosty72 <72458742+Frosty72@users.noreply.github.com>
2021-03-24 10:54:22 -0700

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -63,7 +63,7 @@ public class Robot extends TimedRobot {
    
   public void joyStickVaratation() {
     //gets joysitck value, set value percentage to left trigger state. Pushed = 75% throttle unpushed = 50% throttle
-    boolean isPressed = rightStick.getRawButton(2);
+    boolean isPressed = rightStick.getRawButton(3);
       if(isPressed) {
         stickSpeed = 0.75;
       }
